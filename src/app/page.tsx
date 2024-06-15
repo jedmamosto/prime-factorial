88a104991436a3388ef18603d93f8db0c7eabaf0
jedmamosto <jedmamosto@gmail.com>
2024-06-15 21:50:45 +0800

    feat: added ui and error checks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import IfPrime from '@/components/IfPrime'
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen ">
+    <main className="flex flex-col items-start h-screen m-4">
       <IfPrime />
     </main>
   )
